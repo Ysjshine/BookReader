6ca94341d5160ddc3958700f74ec2463681856c0
Ysjshine <yushijie@buaa.edu.cn>
2017-05-26 23:58:34 +0800

    look like a complete app eventually

--- app/src/main/java/com/buaa/yushijie/bookreader/Fragments/BookReadFragment.java
+++ app/src/main/java/com/buaa/yushijie/bookreader/Fragments/BookReadFragment.java
@@ -1,15 +1,22 @@
 package com.buaa.yushijie.bookreader.Fragments;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.buaa.yushijie.bookreader.Activities.BookReadingActivity;
 import com.buaa.yushijie.bookreader.R;
@@ -27,6 +34,10 @@ public class BookReadFragment extends Fragment {
     private BookBean currentBook;
     private Activity currentActivity;
     private Book books;
+    private int currentPage = 0;
+    private GestureDetector gesture = null;
+    private int mWebViewHeight;
+    private int mWebViewWidth;
 
     private Handler handler = new Handler(){
         @Override
@@ -34,7 +45,9 @@ public class BookReadFragment extends Fragment {
             books = (Book) msg.obj;
             try {
                 String a = new String(books.getContents().get(2).getData(),"UTF-8");
+               // goToNextPage(mWebView,5);
                 mWebView.loadData(a, "text/html;charset=UTF-8", null);
+                //Log.e("sss", "onCreateView: "+mWebView.getHeight()+" "+mWebView.getContentHeight()+" "+mWebView.getWidth() );
 
             }catch (Exception e){
                 e.printStackTrace();
@@ -56,6 +69,12 @@ public class BookReadFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.book_read_fragment,container,false);
         mWebView = (WebView)v.findViewById(R.id.book_reading_webview);
+        mWebView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new MyWebViewClient());
+        gesture =  new GestureDetector(currentActivity,new GestureListeners());
+        mWebView.setOnTouchListener(new GestureListeners());
+        mWebView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
         getBookData();
         return v;
     }
@@ -74,4 +93,66 @@ public class BookReadFragment extends Fragment {
             }
         }).start();
     }
+
+    private class MyWebViewClient extends WebViewClient{
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            String js = "javascript:function initialize() { " +
+                    "var d = document.getElementsByTagName('body')[0];" +
+                    "var ourH = window.innerHeight; " +
+                    "var ourW = window.innerWidth; " +
+                    "var fullH = d.offsetHeight; " +
+                    "var pageCount = Math.floor(fullH/ourH)+1;" +
+                    "var newW = pageCount*ourW; " +
+                    "d.style.height = (ourH-30)+'px';" +
+                    "d.style.width = newW+'px';" +
+                    "d.style.webkitColumnGap = '2px'; " +
+                    "d.style.padding = 10; " +
+                    "d.style.webkitColumnCount = pageCount;" +
+                    "window.scrollBy("+currentPage+"*ourW,0)"+
+                    "}";
+            view.loadUrl(js);
+           view.loadUrl("javascript:initialize()");
+        }
+    }
+
+    private void goToNextPage(WebView view,int currentPage) {
+        String js = "javascript:function nextPage() { " +
+                "var d = document.getElementsByTagName('body')[0];" +
+                "var ourH = window.innerHeight; " +
+                "var ourW = window.innerWidth; " +
+                "window.scrollBy("+currentPage+"*ourW,0)"+
+                "}";
+        view.loadUrl(js);
+        view.loadUrl("javascript:nextPage()");
+    }
+    private class GestureListeners  extends GestureDetector.SimpleOnGestureListener implements View.OnTouchListener{
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return gesture.onTouchEvent(event);
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            float x = e1.getX()-e2.getX();
+            float y = e1.getY()-e2.getY();
+            if(x>0 && Math.abs(y)<x) {
+                Log.w("ss", "right");
+                //currentPage++;
+                goToNextPage(mWebView,1);
+
+            }
+            else if(x<0 && Math.abs(y)<Math.abs(x)){
+                Log.d("sss", "onFling: left");
+                //currentPage--;
+                goToNextPage(mWebView,-1);
+
+            }
+            return true;
+        }
+    }
+
+
 }
+
